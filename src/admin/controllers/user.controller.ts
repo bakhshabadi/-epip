7413a5624b43342926cf580166c303a7a632e693
bakhshabadi <bakhshabadi.javad@gmail.com>
2021-09-05 20:52:12 +0430

    fix controller

--- src/admin/controllers/user.controller.ts
+++ src/admin/controllers/user.controller.ts
@@ -3,7 +3,8 @@
 // import { BaseController } from "src/@core/controllers/base.controller";
 // import { ApiController } from "src/@core/decorators/swg-ctrl.decorator";
 
-import { ApiController, ApiGetAll, IResponse } from "@epip/crud";
+import { ApiController, ApiDelete, ApiGet, ApiGetAll, ApiPatch, ApiPost, ApiPut, IResponse, IResponseAll } from "@epip/crud";
+import { DeepPartial } from "typeorm";
 import { Users } from "../models/users.model";
 import { UserService } from "../services/user.service";
 
@@ -12,8 +13,28 @@ export class UserController {
   constructor(private readonly service: UserService) {
   }
 
-  @ApiGetAll(Users)
-  public async getAll(): Promise<IResponse<Users>>  {
-    return await this.service.getAll();
+  @ApiGet(Users)
+  public async get(id:number): Promise<IResponse<Users>>  {
+    return await this.service.get(id);
+  }
+
+  @ApiPost(Users)
+  public async post(entity:Users): Promise<IResponse<Users>>  {
+    return await this.service.post(entity);
+  }
+
+  @ApiPut(Users)
+  public async put(id:number,entity:Users): Promise<IResponse<Users>>  {
+    return await this.service.put(id,entity);
+  }
+
+  @ApiPatch(Users)
+  public async patch(id:number,entity:DeepPartial<Users>): Promise<IResponse<DeepPartial<Users>>>  {
+    return await this.service.patch(id,entity);
+  }
+
+  @ApiDelete(Users)
+  public async delete(id:number): Promise<IResponse<Users>>  {
+    return await this.service.delete(id);
   }
 }
