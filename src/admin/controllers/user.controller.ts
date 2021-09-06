37d5cdb838b2166978488692551606b174af0a98
bakhshabadi <bakhshabadi.javad@gmail.com>
2021-09-06 23:35:01 +0430

    add request in ctrl

--- src/admin/controllers/user.controller.ts
+++ src/admin/controllers/user.controller.ts
@@ -1,6 +1,6 @@
 import { ApiController, ApiDelete, ApiGet, ApiGetAll, ApiPatch, ApiPost, ApiPut, IResponse, IResponseAll } from "@epip/crud";
-import { Body, Param } from "@nestjs/common";
-import { DeepPartial } from "typeorm";
+import { Body, Param, Req } from "@nestjs/common";
+import { Request } from "express";
 import { User } from "../models/user.model";
 import { UserService } from "../services/user.service";
 
@@ -10,32 +10,32 @@ export class UserController {
   }
 
   @ApiGetAll(User)
-  public async getAll(): Promise<IResponseAll<User>>  {
+  public async getAll(@Req() req: Request): Promise<IResponseAll<User>>  {
     return await this.service.getAll();
   }
 
   @ApiGet(User,':id')
-  public async get(@Param() param): Promise<IResponse<User>>  {
-    return await this.service.get(param.id);
+  public async get(@Req() req: Request, @Param() param): Promise<IResponse<User>>  {
+    return await this.service.get(req, param.id);
   }
 
   @ApiPost(User)
-  public async post(@Body() entity:User): Promise<IResponse<User>>  {
-    return await this.service.post(entity);
+  public async post(@Req() req: Request, @Body() entity:User): Promise<IResponse<User>>  {
+    return await this.service.post(req, entity);
   }
 
   @ApiPut(User,':id')
-  public async put(@Param() param,@Body() entity:User): Promise<IResponse<User>>  {
-    return await this.service.put(param.id,entity);
+  public async put(@Req() req: Request, @Param() param,@Body() entity:User): Promise<IResponse<User>>  {
+    return await this.service.put(req, param.id,entity);
   }
 
   @ApiPatch(User,':id')
-  public async patch(@Param() param,@Body() entity:User): Promise<IResponse<User>>  {
-    return await this.service.patch(param.id,entity);
+  public async patch(@Req() req: Request, @Param() param,@Body() entity:User): Promise<IResponse<User>>  {
+    return await this.service.patch(req, param.id,entity);
   }
 
   @ApiDelete(User,':id')
-  public async delete(@Param() param): Promise<IResponse<User>>  {
-    return await this.service.delete(param.id);
+  public async delete(@Req() req: Request, @Param() param): Promise<IResponse<User>>  {
+    return await this.service.delete(req, param.id);
   }
 }
